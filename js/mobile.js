55599e01d4fe134662f2a9d8b4e87cd65cd09105
Iris Wang <iriswang1224@163.com>
2017-11-14 15:00:38 +0800

    选择课程页面

--- js/mobile.js
+++ js/mobile.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 	document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
 	// .view 高度
 	$('.view').css('min-height',$(window).height());
-
-
+    //课程list高度
+    $('.class-pannel').css('height', $(window).height() - $('.topbar').innerHeight());
+    $('.enroll .class-pannel').css('height', $(window).height() - $('.topbar').innerHeight() - $('.enroll-btn').innerHeight());
 })
